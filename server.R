ab69868e54ab634cb80321bb9ebc7a8819ffd51f
BastienVe <bastien.vendrame@edu.ece.fr>
2020-11-22 12:59:07 +0100

    Add files
    Frist push

---
+++ server.R
@@ -0,0 +1,294 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(data.table)
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+
+    #AVAILABILITY INFORMATION OVER THE LAST 30 DAYS
+    average_availability <-reactive({ 
+        listings %>% filter(city %in% input$selectCity) %>% group_by(city) %>%
+            summarise(average_availability_30=round(mean(availability_30),2))
+    })
+    
+    average_availability_bedrooms <-reactive({ 
+        listings %>% filter(city %in% input$selectCity) %>% group_by(city,bedrooms) %>%
+            summarise(average_availability_30=round(mean(availability_30),2))
+    })
+    average_availability_type <-reactive({ 
+        listings %>% filter(city %in% input$selectCity) %>% group_by(city,room_type) %>%
+            summarise(average_availability_30=round(mean(availability_30),2))
+    })
+    
+    
+    distribution_availability <-reactive({ listings %>% filter(city %in% input$selectCity) })
+    
+    output$plot1<-renderPlot({
+        if(input$selectCriteria==0){
+            if(input$selectDiagram==1){
+                p<-ggplot(data=average_availability(), aes(x=city, y=average_availability_30,fill=city)) + geom_bar(stat="identity", position=position_dodge())+
+                    geom_text(aes(label=average_availability_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3.5) + scale_fill_brewer(palette="Blues")
+                print(p)
+            
+            }else {
+                p <- ggplot(distribution_availability(), aes(city, availability_30))+ geom_boxplot(aes(fill = city),outlier.shape = NA) +
+                    scale_y_continuous(limits = quantile(listings$availability_30, c(0.1, 0.9), na.rm = T)) + scale_fill_brewer(palette="Blues")
+                print(p)
+            }
+        }else if (input$selectCriteria==1){
+            if(input$selectDiagram==1){
+                p<-ggplot(data=average_availability_bedrooms(), aes(x=city, y=average_availability_30, fill=bedrooms)) + geom_bar(stat="identity", position=position_dodge())+
+                    geom_text(aes(label=average_availability_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3) + scale_fill_brewer(palette="Blues")
+                print(p)  
+            }else{
+                p <- ggplot(distribution_availability(), aes(city, availability_30))+ geom_boxplot(aes(fill = bedrooms),outlier.shape = NA) +
+                    scale_y_continuous(limits = quantile(listings$availability_30, c(0.1, 0.9), na.rm = T)) + scale_fill_brewer(palette="Blues")
+                print(p)
+            }
+        }else if (input$selectCriteria==2){
+            
+            if(input$selectDiagram==1){
+                p<-ggplot(data=average_availability_type(), aes(x=city, y=average_availability_30, fill=room_type)) + geom_bar(stat="identity", position=position_dodge())+
+                    geom_text(aes(label=average_availability_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3) + scale_fill_brewer(palette="Blues")
+                print(p) 
+            }else{
+                p <- ggplot(distribution_availability(), aes(city, availability_30))+ geom_boxplot(aes(fill = room_type),outlier.shape = NA) +
+                    scale_y_continuous(limits = quantile(listings$availability_30, c(0.1, 0.9), na.rm = T)) + scale_fill_brewer(palette="Blues")
+                print(p)
+            }
+        }
+            
+    })
+    
+    #REVENUE INFORMATION DURING THE LAST 30 DAYS
+    average_revenue <-reactive ({
+        listings %>% filter(city %in% input$selectCity) %>%  group_by(city) %>%
+        summarise(average_revenue_30=round(mean(revenue_30),2))
+    })
+    average_revenue_bedrooms <-reactive ({
+        listings %>% filter(city %in% input$selectCity) %>%  group_by(city,bedrooms) %>%
+            summarise(average_revenue_30=round(mean(revenue_30),2))
+    })
+    average_revenue_type <-reactive ({
+        listings %>% filter(city %in% input$selectCity) %>%  group_by(city,room_type) %>%
+            summarise(average_revenue_30=round(mean(revenue_30),2))
+    })
+    distribution_revenue <-reactive({ listings %>% filter(city %in% input$selectCity) })
+
+    output$plot2<-renderPlot({
+        if(input$selectCriteria==0){
+            if(input$selectDiagram==1){
+            p<-ggplot(data=average_revenue(), aes(x=city, y=average_revenue_30, fill=city)) + geom_bar(stat="identity", position=position_dodge())+
+                geom_text(aes(label=average_revenue_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3.5) + scale_fill_brewer(palette="Blues")
+            print(p)
+            }else {
+                p <- ggplot(distribution_revenue(), aes(city, revenue_30)) + geom_boxplot(aes(fill = city), outlier.shape = NA) +
+                    scale_y_continuous(limits = quantile(listings$revenue_30, c(0.1, 0.9), na.rm = T)) + scale_fill_brewer(palette="Blues")
+                print(p)
+            }
+            
+        }else if (input$selectCriteria==1){
+            if(input$selectDiagram==1){
+                p<-ggplot(data=average_revenue_bedrooms(), aes(x=city, y=average_revenue_30, fill=bedrooms)) + geom_bar(stat="identity", position=position_dodge())+
+                    geom_text(aes(label=average_revenue_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3) + scale_fill_brewer(palette="Blues")
+                print(p)
+            }else{
+                p <- ggplot(distribution_revenue(), aes(city, revenue_30))
+                p + geom_boxplot(aes(fill = bedrooms), outlier.shape = NA) +
+                    scale_y_continuous(limits = quantile(listings$revenue_30, c(0.1, 0.9), na.rm = T)) + scale_fill_brewer(palette="Blues")
+            }
+            
+        }else if (input$selectCriteria==2){
+            if(input$selectDiagram==1){
+                p<-ggplot(data=average_revenue_type(), aes(x=city, y=average_revenue_30, fill=room_type)) + geom_bar(stat="identity", position=position_dodge())+
+                    geom_text(aes(label=average_revenue_30), vjust=1.6, color="white",position = position_dodge(0.9), size=2.6) + scale_fill_brewer(palette="Blues")
+                print(p)
+                
+            }else{
+                p <- ggplot(distribution_revenue(), aes(city, revenue_30))
+                p + geom_boxplot(aes(fill = room_type), outlier.shape = NA) +
+                    scale_y_continuous(limits = quantile(listings$revenue_30, c(0.1, 0.9), na.rm = T))+ scale_fill_brewer(palette="Blues")
+            }
+            
+        }
+    })
+    
+    #TOP INFORMATION
+    
+    top_price <- reactive ({
+        head(listings %>% select("country","city","listing_url","room_type","bedrooms","price_30") %>% filter(city==input$selectCityTop & price_30>0) %>% arrange(price_30) %>% group_by(country,city,listing_url,room_type,bedrooms,price_30) %>% summarize(), 5)
+    })
+    top_revenue <- reactive ({
+        head(listings %>% select("country","city","listing_url","room_type","bedrooms","revenue_30") %>% filter(city==input$selectCityTop)%>% group_by(country,city,listing_url,room_type,bedrooms,revenue_30) %>% summarize() %>% arrange(desc(revenue_30)) , 5) 
+    })
+    top_available <- reactive ({
+        head(listings %>% select("country","city","listing_url","room_type","bedrooms","availability_30") %>% filter(city==input$selectCityTop)%>% group_by(country,city,listing_url,room_type,bedrooms,availability_30) %>% summarize() %>% arrange(desc(availability_30)) , 5) 
+    })
+    top_nights <- reactive ({
+        head(listings %>% select("country","city","listing_url","room_type","bedrooms","maximum_nights") %>% filter(city==input$selectCityTop)%>% group_by(country,city,room_type,bedrooms,maximum_nights) %>% summarize() %>% arrange(desc(maximum_nights)) , 5) 
+    })
+    
+    output$top_price <- renderTable(
+        top_price()
+    )
+    output$top_revenue <- renderTable(
+        top_revenue()
+    )
+    output$top_available <- renderTable(
+        top_available()
+    )
+    output$top_nights <- renderTable(
+        top_nights()
+    )
+    
+    
+    #PIE INFORMATION
+    
+    #Dynamic Widgets selection
+    filter_country <- reactive ({
+        listings %>% filter(country %in% input$selectCountryOne)  %>% group_by(city) %>% summarise()
+    })
+    
+    output$city_selection <- renderUI ({
+        selectInput("selectCityOne", h4("Choose the city you want to analyze : "), 
+                    choices = c(unique(filter_country()[[1]])))
+    })
+    
+    
+    min_date <- reactive({
+        listings %>% filter(city %in% input$selectCityOne) %>% group_by(data_date)%>% summarise() %>% arrange(data_date)
+    })
+    max_date <- reactive({
+        listings %>% filter(city %in% input$selectCityOne) %>% group_by(data_date)%>% summarise() %>% arrange(desc(data_date))
+    })
+    
+    output$date_range <- renderText({
+        
+        paste("Date Range for", input$selectCityOne, ":", min_date()[1,1], "to", max_date()[1,1])
+    })
+    
+    average_availability_One <-reactive({ 
+        listings %>% filter(city == input$selectCityOne) %>% group_by(city) %>%
+            summarise(average_availability_30=round(mean(availability_30),2))
+    })
+    average_availability_bedrooms_One <-reactive({ 
+        listings %>% filter(city == input$selectCityOne) %>% group_by(city,bedrooms) %>%
+            summarise(average_availability_30=round(mean(availability_30),2))
+    })
+    average_availability_type_One <-reactive({ 
+        listings %>% filter(city == input$selectCityOne) %>% group_by(city,room_type) %>%
+            summarise(average_availability_30=round(mean(availability_30),2))
+    })
+    
+    output$plot3 <- renderPlot({
+        if(input$selectOneCriteriaAvailability == 0){
+            p<-ggplot(data=average_availability_One(), aes(x=city, y=average_availability_30, fill=city)) + geom_bar(stat="identity", position=position_dodge())+
+                geom_text(aes(label=average_availability_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3) +scale_fill_brewer(palette="Blues")
+            print(p)
+        }else if (input$selectOneCriteriaAvailability == 1){
+            p<-ggplot(data=average_availability_bedrooms_One(), aes(x=bedrooms, y=average_availability_30, fill=bedrooms)) + geom_bar(stat="identity", position=position_dodge())+
+                geom_text(aes(label=average_availability_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3) +scale_fill_brewer(palette="Blues")
+            print(p)  
+            
+        }else {
+            p<-ggplot(data=average_availability_type_One(), aes(x=room_type, y=average_availability_30, fill=room_type)) + geom_bar(stat="identity", position=position_dodge())+
+                geom_text(aes(label=average_availability_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3) +scale_fill_brewer(palette="Blues")
+            print(p) 
+        }
+        
+    })
+    
+    average_revenue_One <-reactive({ 
+        listings %>% filter(city == input$selectCityOne) %>% group_by(city) %>%
+            summarise(average_revenue_30=round(mean(revenue_30),2))
+    })
+    average_revenue_bedrooms_One <-reactive({ 
+        listings %>% filter(city == input$selectCityOne) %>% group_by(city,bedrooms) %>%
+            summarise(average_revenue_30=round(mean(revenue_30),2))
+    })
+    average_revenue_type_One <-reactive({ 
+        listings %>% filter(city == input$selectCityOne) %>% group_by(city,room_type) %>%
+            summarise(average_revenue_30=round(mean(revenue_30),2))
+    })
+    
+    output$plot4 <- renderPlot({
+        if(input$selectOneCriteriaRevenue == 0){
+            p<-ggplot(data=average_revenue_One(), aes(x=city, y=average_revenue_30, fill=city)) + geom_bar(stat="identity", position=position_dodge())+
+                geom_text(aes(label=average_revenue_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3) +scale_fill_brewer(palette="Blues")
+            print(p)
+        }else if (input$selectOneCriteriaRevenue == 1){
+            p<-ggplot(data=average_revenue_bedrooms_One(), aes(x=bedrooms, y=average_revenue_30, fill=bedrooms)) + geom_bar(stat="identity", position=position_dodge())+
+                geom_text(aes(label=average_revenue_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3)+scale_fill_brewer(palette="Blues")
+            print(p) 
+            
+        }else {
+            p<-ggplot(data=average_revenue_type_One(), aes(x=room_type, y=average_revenue_30, fill=room_type)) + geom_bar(stat="identity", position=position_dodge())+
+                geom_text(aes(label=average_revenue_30), vjust=1.6, color="white",position = position_dodge(0.9), size=3)+scale_fill_brewer(palette="Blues")
+            print(p)
+            
+        }
+    })
+    
+    room_type_pie <- reactive ({
+        listings %>% filter(city %in% input$selectCityOne) %>% group_by(room_type) %>% summarise(count=n()) %>% transmute(room_type, percent = round(count/sum(count),3)*100)
+        })
+    output$pie_room_type <- renderPlot({
+        p<-ggplot(data=room_type_pie(), aes(x=input$selectCityOne, y=percent,fill=room_type)) + geom_bar(stat="identity")+
+            geom_text(aes(label=percent), vjust=1.6, color="black",position = position_dodge(0.9), size=3.5)
+        pie <- p + coord_polar("y", start=0) +scale_fill_brewer(palette="Blues")
+        print(pie)
+    })
+    
+    bedrooms_pie <- reactive ({
+        listings %>% filter(city %in% input$selectCityOne) %>% group_by(bedrooms) %>% summarise(count=n())%>% transmute(bedrooms, percent = round(count/sum(count),3)*100)
+    })
+    output$pie_bedrooms <- renderPlot({
+        p<-ggplot(data=bedrooms_pie(), aes(x=input$selectCityOne, y=percent,fill=bedrooms)) + geom_bar(stat="identity")+
+            geom_text(aes(label=percent), vjust=1.6, color="black",position = position_dodge(0.9), size=2.9)
+        pie <- p + coord_polar("y", start=0) +scale_fill_brewer(palette="Blues")
+        print(pie)
+    })
+    
+    #LEAFLET MAP
+    leaflet_map <-reactive ({
+        if(is.null(input$selectTypeMap) & is.null(input$selectBedroomMap)){
+            listings %>% filter(city %in% input$selectCityMap & price_30 >= input$selectPrice[1] & price_30 <= input$selectPrice[2])
+            
+        }else if(is.null(input$selectTypeMap) & !is.null(input$selectBedroomMap)){
+            listings %>% filter(city %in% input$selectCityMap & price_30 >= input$selectPrice[1] & price_30 <= input$selectPrice[2] & bedrooms %in% input$selectBedroomMap)
+        }else if(!is.null(input$selectTypeMap) & is.null(input$selectBedroomMap)){
+            listings %>% filter(city %in% input$selectCityMap & price_30 >= input$selectPrice[1] & price_30 <= input$selectPrice[2] & room_type %in% input$selectTypeMap)
+        }else{
+            listings %>% filter(city %in% input$selectCityMap & price_30 >= input$selectPrice[1] & price_30 <= input$selectPrice[2] & room_type %in% input$selectTypeMap & bedrooms %in% input$selectBedroomMap)
+        }
+        
+    })
+    
+    popup <- reactive({
+        paste(sep = " ",
+              "<span style = 'font-weight: bold;'>Room type :</span>", leaflet_map()$room_type, "<br/>",
+              "<span style = 'font-weight: bold;'>Number of bedrooms :</span>", leaflet_map()$bedrooms, "<br/>",
+              "<span style = 'font-weight: bold;'>Neighborhood :</span>", leaflet_map()$neighbourhood_cleansed, "<br/>",
+              "<span style = 'font-weight: bold;'>URL :</span>", leaflet_map()$listing_url, "<br/><br/>",
+              "<span style = 'font-weight: bold;'>Price :</span>", leaflet_map()$price, "<br/>"
+        )
+    })
+    output$mymap <- renderLeaflet({
+        m <- leaflet(leaflet_map()) %>% addTiles() %>% addMarkers(lng=leaflet_map()$longitude, lat=leaflet_map()$latitude, clusterOptions = markerClusterOptions(),popup = popup())
+        print(m)
+    })
+
+})
---
+++ ui.R
@@ -0,0 +1,203 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(maps)
+library(mapproj)
+library(leaflet)
+library(shinydashboard)
+
+
+suppressPackageStartupMessages(library(googleVis))
+
+
+
+shinyUI( dashboardPage(skin = "yellow",
+    
+    dashboardHeader(title = "Airbnb Analysis"),
+    
+    dashboardSidebar(
+        
+        sidebarMenu(
+            menuItem("Home", tabName = "home", icon = icon("home")),
+            menuItem("Comparator", tabName = "comparing_cities", icon = icon("dashboard")),
+            menuItem("top", tabName = "top", icon = icon("kiss-wink-heart")),
+            menuItem("My city", tabName = "city", icon = icon("thumbtack")),
+            menuItem("Map", tabName = "map", icon = icon("map"))
+            
+        )
+        
+    ),
+    
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "home",
+                    h1("Welcome on our Dash Board ! ", style="font-family: 'Gill Sans', sans-serif; font-weight:bold;"),
+                    h2("Presentation", style="font-family: 'Gill Sans', sans-serif;text-decoration: underline;"),
+                    p(
+                        "We are 3 students working on an Analytic project. We designed for you a dashboard which allows you to visualize some Airbnb data !"
+                    ),
+                    p(
+                        "This is a simple shiny application. Choose cities you want to compare, select the place you want to visit. Enjoy ! "
+                    ),
+                    p(
+                        "Fist, let's have a look on some instructions ! "
+                    ),
+                    h3("Comparing different cities", style="font-family: 'Gill Sans', sans-serif;text-decoration: underline;"),
+                    p(
+                        "On the first item ('Comparator'), you will be to compare different cities."
+                    ),
+                    p(
+                        "First, select all the cities you want to analyze. You'll see some Graphs concerning availabilty and revenue about all the appartments in the cities choosen.  "
+                    ),
+                    p(
+                        img(src = "comparator.png", height = 300),
+                        align = "center"
+                    ),
+                    h3("Deep dive into one city", style="font-family: 'Gill Sans', sans-serif;text-decoration: underline;"),
+                    p(
+                        "Your choice for the next hollydays may be already done. No problem, you will be able to get more information about this city."
+                    ),
+                    p(
+                        "Select the city you're interested in and have a look on the information. House size ? Room type ? The city will no longer have secret for you !"
+                    ),
+                    p(
+                        img(src = "city.png", height = 300),
+                        align = "center"
+                    ),
+                    
+        ),
+            tabItem(tabName = "comparing_cities",
+                fluidRow(
+                    box(
+                        title = "Input", status = "warning", solidHeader = TRUE,
+                        helpText("In this little dashboard, you will be able to compare some cities. Choose them carefully and add some dimensions to your analyze if you want. Enjoy !"),
+                        checkboxGroupInput("selectCity", 
+                                           label="Select cities you want to analyze : ", 
+                                           choices = c(unique(listings$city)), inline = TRUE)
+                    ),
+                    box(
+                        title = "Criteria", status = "warning", solidHeader = TRUE,
+                        selectInput("selectCriteria", label ="Choose one of these criteria : ", 
+                                    choices = list("No specific criteria"=0,"Number of bedrooms" = 1, "Type of the room" = 2), selected = 0),
+                        
+                        selectInput("selectDiagram", h4("Wich information do you want to see : "), 
+                                    choices = list("Average information (histogram)" = 1, "Distribution information (Box)" = 2), selected = 1)
+                    )
+                ),
+                fluidRow(
+                    box(
+                        title = "Availability information", status = "primary", solidHeader = TRUE,
+                        plotOutput("plot1",height = 250)
+                    ),
+                    box(
+                        title = "Revenue Information", status = "primary", solidHeader = TRUE,
+                        plotOutput("plot2",height = 250)
+                    )
+                )
+            ),
+            tabItem(tabName = "top",
+                    fluidRow(
+                        box(
+                            title = "Input", status = "warning", solidHeader = TRUE,
+                            selectInput("selectCityTop", label = "Choose your city :", 
+                                        choices = c(unique(listings$city)))
+                        )
+                    ),
+                    fluidRow(
+                        box(
+                            title = "Top 5 cheaper places", status = "primary", solidHeader = TRUE,
+                            column( 12,align="center" ,tableOutput("top_price"))
+                            
+                            
+                        ),
+                        box(
+                            title = "Top 5 revenues", status = "primary", solidHeader = TRUE,
+                            column( 12,align="center" ,tableOutput("top_revenue"))
+                        )
+     
+                    ),
+                    fluidRow(
+                        box(
+                            title = "Top 5 available places", status = "primary", solidHeader = TRUE,
+                            column( 12,align="center" ,tableOutput("top_available"))
+                        ),
+                        box(
+                            title = "Top 5 maximum nights", status = "primary", solidHeader = TRUE,
+                            column( 12,align="center" ,tableOutput("top_nights"))
+                        )
+                    )
+                    
+                    ),
+            tabItem(tabName = "city",
+                fluidRow(
+                    box(
+                        title = "Inputs", status = "warning", solidHeader = TRUE,
+                        helpText("If you want information about only one city, you're in the right place ! "),
+                        selectInput("selectCountryOne",label = "Choose the country : ", 
+                                    choices = c(unique(listings$country)), selected = "france"),
+                        htmlOutput("city_selection"),
+                        textOutput("date_range"),
+                        htmlOutput("date_selection")
+                    )
+                ),
+                fluidRow(
+                    box(
+                        title = "Room type", status = "primary", solidHeader = TRUE,
+                        plotOutput("pie_room_type"),
+                    ),
+                    box(
+                        title = "House size", status = "primary", solidHeader = TRUE,
+                        plotOutput("pie_bedrooms")
+                    )
+                ),
+                fluidRow(
+                    box(
+                        title = "Availability information", status = "primary", solidHeader = TRUE,
+                        selectInput("selectOneCriteriaAvailability", label = "Choose one of these criteria : ", 
+                                    choices = list("No specific criteria"=0,"Number of bedrooms" = 1, "Type of the room" = 2), selected = 0),
+                        plotOutput("plot3",height = 250)
+                    ),
+                    box(
+                        title = "Revenue information", status = "primary", solidHeader = TRUE,
+                        selectInput("selectOneCriteriaRevenue", label="Choose one of these criteria : ", 
+                                    choices = list("No specific criteria"=0,"Number of bedrooms" = 1, "Type of the room" = 2), selected = 0),
+                        plotOutput("plot4",height = 250)
+                    )
+                ),
+            ),
+            tabItem(tabName = "map",
+                fluidRow(
+                       box(
+                           title = "Leaflet Map", status = "primary",width = 8, height = 850, solidHeader = TRUE,
+                           leafletOutput("mymap",width="100%", height = "750px")
+                       ),
+                       box(
+                           title = "Inputs", status = "warning",width = 4, solidHeader = TRUE,
+                           helpText("You want to learn more about a city ? Let's visualize a map to get more information !"),
+                           selectInput("selectCityMap", h4("Choose one of these city : "), 
+                                       choices = c(unique(listings$city)), selected = "mallorca"),
+                           helpText("Add granularity to your research :"),
+                           sliderInput("selectPrice", label = h3("Price range"), min = 0, 
+                                       max = 500, value = c(0, 470)),
+                           box(
+                               checkboxGroupInput("selectTypeMap", h4("Which type of rooms ?"), choices = c(unique(listings$room_type)), selected = "Hotel room")
+                           ),
+                           box(
+                               checkboxGroupInput("selectBedroomMap", h4("How many bedrooms ?"), choices = c(unique(listings$bedrooms)),selected ="2")
+                           )
+                           
+                       )
+                )
+            )
+        )
+    )
+))
+
+
